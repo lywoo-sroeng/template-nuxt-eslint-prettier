dc1db8382f3005027657caecf510987bcfba14dd
lywoo <lywoo.sroeng@gmail.com>
2024-08-07 23:12:17 +0700

    chore: auto import components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  components: [
+    {
+      path: './components/',
+      extensions: ['.vue'],
+      prefix: undefined,
+      pathPrefix: false,
+    },
+  ],
   modules: ['@nuxtjs/tailwindcss'],
 })
